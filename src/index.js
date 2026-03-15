0ee2d9355e6ee0b2271bdd5e6963df82991a65a1
Tushar Mandhare <tmandhare24@gmail.com>
2026-03-15 19:53:53 +0530

    Configure production deployment and serving static files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const http = require('http');
@@ -52,8 +53,18 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/admin', require('./routes/adminRoutes'));
 app.use('/api/reports', require('./routes/reportRoutes'));
 
-app.get('/', (req, res) => {
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api', (req, res) => {
     res.send('Restaurant Billing System API is running...');
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Database Sync and Server Start
